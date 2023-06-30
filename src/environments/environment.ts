26d0931fc1ffb67a4083781b15f9e170037db4d9
Kamlesh Mundel <kamlesh@techuz.com>
2023-06-30 17:08:23 +0530

    re-arrange steps test

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,5 +1,5 @@
 export const environment = {
     API_URL: 'https://api.openai.com/v1/chat/completions',
     API_KEY: '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',
-    MODEL_PROMPT: 'You a bot who can only do chat.'
+    MODEL_PROMPT: 'You a bot who can only do chat'
 }
