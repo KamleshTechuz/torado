ccf90fc27478e7a847da3e782b5069a28309bbe9
KamleshTechuz <kamlesh@techuz.com>
2023-08-25 16:30:36 +0530

    .env

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,5 +1,5 @@
 export const environment = {
     API_URL: 'https://api.openai.com/v1/chat/completions',
-    API_KEY: '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',
+    API_KEY: process.env.API_KEY,
     MODEL_PROMPT: 'You a bot who can only do chat'
 }
